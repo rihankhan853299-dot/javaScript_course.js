a78100a223777a43c6191557128b1e2656b514c1
Rihan Khan <rihankhan853299@gmail.com>
2026-02-19 02:50:24 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,21 @@
+console.log("loops in javaScript")
+
+// loop use for repeatative tasks 
+
+/* types of loop*/
+
+// 1.. for loop
+
+/* 
+syntax 
+for (initialisation, condition , updation) {
+      // code
+}
+*/
+
+// example
+
+for (let i=1;i<10;i++) {
+    console.log()
+    console.log(i*2)
+}

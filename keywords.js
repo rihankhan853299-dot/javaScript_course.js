46d9f4ceec4c6da4301ca909bb30ef07fc896c31
Rihan Khan <rihankhan853299@gmail.com>
2026-03-11 01:25:50 +0530

    Add files via upload

---
+++ keywords.js
@@ -0,0 +1,16 @@
+/* what is keywords ?
+
+// aise words jika koi special meaning h in javaScript programming language or Other programming language. vo sab keywords kajlate h
+
+// easy definiation of keywords---> jiss word ka koi matlab vo sare words keywords h
+
+jaise var, let, cost, function ye sab keywords h
+
+var,let,cost variable declare karne k kile use hote h.
+function---> function keyword ka use function banane k liye hota h function keyword k bina hum function declare nhi kar sakte
+
+IMP 
+1.JvaScript mei total 48 keywirds Available h.
+
+2..jabki let and const ES6 vesion(2015) mein incloud hue the.
+*/

615680755f1655bdcbbacebbd6013924406e8e87
Rihan Khan <rihankhan853299@gmail.com>
2026-03-11 01:24:28 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,39 @@
+/* what is variable?
+
+variable is the name of merory location that use for store the values(data) 
+variable data store karne k liye use hote h 
+
+IMP
+javaScript mein variable ko javaaScript mei 3 tarah se declare kya jata h 
+
+1..VAR keyword ka use karke 
+2..let keywords ka use karke
+3..const keywords ka use karke 
+
+SYNTAX OF DECLARATION & INITIALIZATION  OF VARIABLE
+
+DECLARATION OF VARIABLE
+keyword_name  variable_name---> this is declaration of variable
+ 
+Exampe 
+let a---> abhi keval variable Declare hua initialize nhi hua h
+
+DECLARATION & INITIALIZATION  OF VARIABLE
+
+keyword_name variable_name =10;
+
+EXAMPLE
+let a=10---> iss line m variable declare bhi hua and initialize bhi hua h.
+
+# NAMING RULES OF VARIABLES.
+
+1..not space Allowed between the Name of variable
+2..start mei digit NHI Use kar sakte
+3..$ and __ ke alava koi bhi Special Character Allow NHI h.
+4..variable ko kisi bhi keyword naam NHI de sakte
+*/
+
+// a simple program of variable
+
+let a=10
+console.log("value of a=",a);

ad813279ae101a2e0b3666fa8f96be4a54ee6e41
Rihan Khan <rihankhan853299@gmail.com>
2026-03-11 01:25:26 +0530

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,9 @@
+/* javaScript created BERDERN EICH in DECEMBER 1995
+
+IMP
+1.. javaScript ko pahle MOCHA or LIVESCRIPT ke name se jante the.
+2.. JvaScript ko only 10-15 dino mei banaya gaya tha.
+
+-->BERDERN EICH  NetScap(NetScape is a Browser) Compomy m kaam karta tha 
+
+*/
